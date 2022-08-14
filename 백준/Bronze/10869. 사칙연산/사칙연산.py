54812234974171c03cb1dd4510b44fd0fbeae6bc
hanuuul <100219566+hanuuul@users.noreply.github.com>
2022-08-14 18:05:57 +0900

    [Bronze V] Title: 사칙연산, Time: 56 ms, Memory: 29284 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,12 @@
+#두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+
+a, b = input().split()
+
+A = int(a)
+B = int(b)
+
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
