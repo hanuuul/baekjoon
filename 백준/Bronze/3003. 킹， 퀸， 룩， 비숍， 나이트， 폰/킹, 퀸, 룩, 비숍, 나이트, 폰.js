d1498236e78db8368f1165e54495d9f73404bdbe
hanuuul <100219566+hanuuul@users.noreply.github.com>
2022-08-16 17:25:31 +0900

    Update 킹, 퀸, 룩, 비숍, 나이트, 폰.js

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹, 퀸, 룩, 비숍, 나이트, 폰.js
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹, 퀸, 룩, 비숍, 나이트, 폰.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var input = fs.readFileSync('/dev/stdin').toString().split(' ');
 
-chess = [1, 1, 2, 2, 2, 8]
+const chess = [1, 1, 2, 2, 2, 8]
 
 for(i=0; i<6; i++){
     console.log(chess[i] - input[i])
