7289dc497eed7b372b6ae6ab6fa44401b2fc5085
hanuuul <100219566+hanuuul@users.noreply.github.com>
2022-08-19 12:24:09 +0900

    Update 사분면 고르기.py

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -1,11 +1,13 @@
 x = int(input())
 y = int(input())
-
-if x*y > 0 and x > 0:
-    print(1)
-elif x*y > 0 and x < 0:
-    print(3)
-elif x*y < 0 and x < 0:
-    print(2)
+ 
+if x*y > 0:
+    if x > 0:
+        print(1)
+    else:
+        print(3)
 else:
-    print(4)
+    if x < 0:
+        print(2)
+    else:
+        print(4)
