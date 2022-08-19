8fc0f45149c3f9a4c93b3222611306994bc7c423
hanuuul <100219566+hanuuul@users.noreply.github.com>
2022-08-19 12:17:46 +0900

    Update 알람 시계.py

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,11 +1,16 @@
-h, m = input().split()
+H, M = list(map(int, input().split()))
 
-H = int(h)
-M = int(m)
+# 일단 M에서 45분 뺌
+M -= 45
 
-if M>=45:
-    print(H, M-45)
-elif M<45 and H>0:
-    print(H-1, M+15)
-elif M<45 and H==0:
-    print(23, M+15)
+if(M >= 0):
+	# M이 0보다 크거나 같으면 그냥 그대로 출력
+    print(H, M)
+else:
+	# M이 0보다 작으면? H가 1씩 줄어듦 그리고 줄어든 1=60분은 M에게 넘겨줌
+    # 근데 만약 H가 0이라면? -1이 아니고 23이 되어야함 (24시 - 1)
+    M += 60
+    if(H > 0):
+    	print(H-1, M)
+    else:
+    	print(23, M)
